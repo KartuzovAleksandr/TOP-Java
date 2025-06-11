b70866145589be459086af7f26bcc7eeaf5b0aec
Kartuzov <kartuzovaleksandr@yandex.ru>
2025-06-11 15:41:57 +0300

    Module

---
+++ DemoModule/src/main/java/top/academy/Main.java
@@ -0,0 +1,9 @@
+package top.academy;
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello module!");
+        JOptionPane.showMessageDialog(null, "Hello Demo Module!");
+    }
+}
---
+++ src/Lesson06/AbstractInterface.java
@@ -0,0 +1,38 @@
+package Lesson06;
+
+public class AbstractInterface {
+    public static void main(String[] args) {
+        Employee p1 = new Employee("Вася", "Сбер");
+        Employee p2 = new Employee("Петя", "ВТБ");
+        p1.display();
+        p2.display();
+        p1.print();
+    }
+    interface Sotrudnik {
+        public String Family = "Иванов";
+        void print();
+    }
+    static abstract class Person {
+        public String Name;
+        Person(String name) {
+            Name = name;
+        }
+        public abstract void display();
+    }
+    static class Employee extends Person implements Sotrudnik {
+        public String Bank;
+        Employee(String name, String company) {
+            super(name);
+            Bank = company;
+        }
+        @Override
+        public void display() {
+            System.out.println("Имя = " + Name + " Компания = " + Bank);
+        }
+
+        @Override
+        public void print() {
+            System.out.println("Имя = " + Name + " Фамилия = " + Family);
+        }
+    }
+}
