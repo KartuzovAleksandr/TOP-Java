a7effbcfcce2cc382d3d66887710590fe520f757
Kartuzov <kartuzovaleksandr@yandex.ru>
2025-06-18 17:46:12 +0300

    Lesson09 Generics, Records

---
+++ src/Lesson09/GenericAccount.java
@@ -0,0 +1,25 @@
+package Lesson09;
+
+public class GenericAccount {
+    public static void main(String[] args) {
+        Account<String, Integer> acc1 = new Account<>("2345", 5000);
+        String acc1Id = acc1.getId();
+        System.out.println(acc1Id);
+
+        Account<Integer, Double> acc2 = new Account<>(2345, 50.00);
+        System.out.println(acc2.getId() + " : "  + acc2.getSum());
+    }
+}
+class Account<T, S> {
+    private T id;
+    private S sum;
+
+    Account(T id, S sum) {
+        this.id = id;
+        this.sum = sum;
+    }
+
+    public T getId() { return id; }
+    public S getSum() { return sum; }
+    public void setSum(S sum) { this.sum = sum; }
+}
---
+++ src/Lesson09/RecordPerson.java
@@ -0,0 +1,45 @@
+package Lesson09;
+
+public class RecordPerson {
+    public static void main (String[] args){
+        Person tom = new Person("Tom",  36);
+
+        System.out.println(tom.name());     // Tom
+        System.out.println(tom.age());      // 36
+        System.out.println(tom.hashCode());
+
+        Person bob = new Person("Петя", 21);
+        Person tomas = new Person("Вася", 36);
+        System.out.println(tomas); // toString() даже не нужен
+        System.out.println(tom.equals(bob));    // false
+        System.out.println(tom.equals(tomas));  // true
+    }
+}
+record Person(String name, int age) { }
+
+//class Person {
+//    private final String name;
+//    private final int age;
+//
+//    Person(String name, int age) {
+//        this.name = name;
+//        this.age = age;
+//    }
+//
+//    String name() { return name; }
+//    int age() { return age; }
+//
+//    public boolean equals(Object o) {
+//        if (!(o instanceof Person)) return false;
+//        Person other = (Person) o;
+//        return other.name == name && other.age == age;
+//    }
+//
+//    public int hashCode() {
+//        return Objects.hash(name, age);
+//    }
+//
+//    public String toString() {
+//        return String.format("Person[name=%s, age=%d]", name, age);
+//    }
+//}
