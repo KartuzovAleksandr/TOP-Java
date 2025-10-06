3bef2a34dd5c25e66e9d38eed9d6e1cd3222bbaf
Sanya <kartuzovaleksandr@yandex.ru>
2025-10-06 17:26:36 +0300

    Lesson10

--- src/Lesson10/FunctionalInterface.java
+++ src/Lesson10/FunctionalInterface.java
@@ -2,10 +2,45 @@
 interface Calculator {
     int calculate(int a, int b);
 }
-public void main() {
+// делаем реализацию ФИ с помощью лямбды (анонимный метод)
+void main() {
         // Используем лямбду
-        Calculator add = (a, b) -> a + b;
+        Calculator add = (a, b) -> a + b; // Integer::sum
         Calculator multiply = (a, b) -> a * b;
         System.out.println(add.calculate(2, 3));     // 5
         System.out.println(multiply.calculate(2, 3)); // 6
 }
+
+// полное определение
+//void main(String[] args) {
+//    Operation op = Operations::add;
+//    System.out.println(op.execute(5, 4));       // 9
+//    op = Operations::mul;
+//    System.out.println(op.execute(5, 4));       // 20
+//}
+//interface Operation{
+//    int execute(int x, int y);
+//}
+//class Operations{
+//    static int add(int a, int b){ return a + b; }
+//    static int mul(int a, int b){ return a * b; }
+//}
+
+// передача ссылки через параметр
+//void main(String[] args) {
+//    doOperation(10, 4, Operations::add);  // 14
+//    doOperation(10, 4, Operations::mul);  // 40
+//}
+//
+//static void doOperation(int a, int b, Operation op)
+//{
+//    System.out.println(op.execute(a, b));
+//}
+//}
+//interface Operation{
+//    int execute(int x, int y);
+//}
+//class Operations{
+//    static int add(int a, int b){ return a + b; }
+//    static int mul(int a, int b){ return a * b; }
+//}
--- src/RandomTicket.java
+++ src/RandomTicket.java
@@ -3,15 +3,16 @@ import static java.lang.IO.println;
 // import java.security.SecureRandom;
 public void main() {
     int Students = 13;
-    int start = 65;
-    int end = 111;
+    int start = 1;
+    int end = 89;
     int Questions = end - start + 1;
-    int q1, q2;
+    int q1, q2, q3;
     // Random r = new Random();
     SecureRandom r = new SecureRandom();
     for (int i = 1; i <= Students; i++) {
         q1 = r.nextInt(Questions) + start;
         q2 = r.nextInt(Questions) + start;
-        println(i + ") " + q1 + ", " + q2);
+        q3 = r.nextInt(Questions) + start;
+        println(i + ") " + q1 + ", " + q2 + ", " + q3 );
     }
 }
