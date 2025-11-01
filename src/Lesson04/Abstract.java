051b7fc3c7847e849d7c69bca144636cb3c223d8
Sanya <kartuzovaleksandr@yandex.ru>
2025-11-01 16:42:06 +0300

    Lesson04

--- src/Lesson04/Abstract.java
+++ src/Lesson04/Abstract.java
@@ -1,13 +1,18 @@
 // абстрактный класс фигуры
-abstract static class Shape{
+abstract class Shape {
     int x; // x-координата точки
     int y; // y-координата точки
     Shape(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    void print() {
-        IO.println("Геометрическая фигура"); }
+    abstract void print();
+    // абстрактный метод для вычисления периметра
+    abstract double getPerimeter();
+    // абстрактный метод для вычисления площади
+    abstract double getArea();
+//    void print() {
+//        IO.println("Геометрическая фигура"); }
 }
 // производный класс прямоугольника
 class Rectangle extends Shape
@@ -28,6 +33,16 @@ class Rectangle extends Shape
                 width,
                 height);
     }
+
+    @Override
+    double getPerimeter() {
+        return this.width * 2 + this.height * 2;
+    }
+
+    @Override
+    double getArea() {
+        return this.width * this.height;
+    }
 }
 // производный класс круга
 class Circle extends Shape
@@ -42,14 +57,28 @@ class Circle extends Shape
     void print() {
         System.out.printf("Круг. Центр: (%d, %d); Радиус: %d\n", x, y, radius);
     }
+
+    @Override
+    double getPerimeter() {
+        return this.radius * Math.PI * 2;
+    }
+
+    @Override
+    double getArea() {
+        return this.radius * Math.PI;
+    }
 }
 
 void main() {
     Shape shape1 = new Rectangle(10, 15, 200, 150);
-    Shape shape2 = new Circle(50, 60, 200);
-
     printShape(shape1);
+    IO.println("Площадь прямоугольника: " + shape1.getArea());
+    IO.println("Периметр прямоугольника: " + shape1.getPerimeter());
+
+    Shape shape2 = new Circle(50, 60, 200);
     printShape(shape2);
+    IO.println("Площадь круга: " + shape2.getArea());
+    IO.println("Периметр круга: " + shape2.getPerimeter());
 }
 
 static void printShape(Shape shape){
